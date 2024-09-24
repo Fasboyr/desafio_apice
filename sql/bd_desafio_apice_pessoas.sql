f03824210cec9084407230975799f7eed6f191dc
Fabio Roberto Pereira Filho <112351137+Fasboyr@users.noreply.github.com>
2024-09-24 15:38:11 -0300

    Adicionando arquivos sql usados como banco de dados
    Colocando no repositório uma pasta com os scripts sql usados para criar o banco de dados usado nesse projeto

---
+++ sql/bd_desafio_apice_pessoas.sql
@@ -0,0 +1,63 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bd_desafio_apice
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pessoas`
+--
+
+DROP TABLE IF EXISTS `pessoas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoas` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `id_cidade` int(11) NOT NULL,
+  `id_bairro` int(11) NOT NULL,
+  `cep` char(9) NOT NULL,
+  `endereco` varchar(150) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `complemento` varchar(50) NOT NULL,
+  `telefone` char(14) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cidade` (`id_cidade`),
+  KEY `fk_bairro` (`id_bairro`),
+  CONSTRAINT `fk_bairro` FOREIGN KEY (`id_bairro`) REFERENCES `bairros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_cidade` FOREIGN KEY (`id_cidade`) REFERENCES `cidades` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoas`
+--
+
+LOCK TABLES `pessoas` WRITE;
+/*!40000 ALTER TABLE `pessoas` DISABLE KEYS */;
+INSERT INTO `pessoas` VALUES (1,'Angela',3,1,'87702850','Rua Alcides Parizoto',532,'Casa','44995863387','angel@outlook.com'),(2,'Bartholomew',1,6,'87705070','Rua Benedito Pereira Tangerino',149,'Apartamento','44976874452','bart.email@gmail.com'),(3,'Carlos',2,2,'87706605','Rua Paulo Antônio da Costa',986,'Casa','44981861135','carlos2002@hotmail.com');
+/*!40000 ALTER TABLE `pessoas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-24 15:33:15
